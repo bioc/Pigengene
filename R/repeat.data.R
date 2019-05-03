d5966d6f65d36596732a60a126def2f7ccd4e864
Habil Zare <habil_zare@yahoo.com>
2019-05-02 20:35:19 -0500

    Pigengene:repeat.data bug fix

--- R/repeat.data.R
+++ R/repeat.data.R
@@ -1,12 +1,17 @@
 repeat.data <- function (Data, times){
     result <- list()
-    if (is.null(rownames(Data))) 
+    if(times < 1){
+        stop("times cannot be <1 !")
+    }
+    if (is.null(rownames(Data)))
         rownames(Data) <- 1:nrow(Data)
     repeated <- Data
-    for (ind in 1:(times - 1)) {
-        Datai <- Data
-        rownames(Datai) <- paste(rownames(Data), ind, sep="-")
-        repeated <- rbind(repeated, Datai)
+    if(times >1){
+        for (ind in 1:(times - 1)) {
+            Datai <- Data
+            rownames(Datai) <- paste(rownames(Data), ind, sep="-")
+            repeated <- rbind(repeated, Datai)
+        }
     }
     result[["Data"]] <- Data
     result[["repeated"]] <- repeated

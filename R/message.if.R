daca2c88b48c90c30b8841c0d5635cc97d3aa66e
Habil Zare <zare@uthscsa.edu>
2021-06-25 15:13:54 -0500

    message.if() in a file

--- R/message.if.R
+++ R/message.if.R
@@ -1,6 +1,11 @@
-message.if <- function(me=NULL, verbose=0){
+message.if <- function(me=NULL, verbose=0, txtFile=NULL, append=TRUE, ...){
     ## Prints the message(s) if verbose is more than 0.
-    if(verbose>0)
-        for(m1 in me)
+    if(verbose>0){
+        for(m1 in me){
             message(m1)
+        }
+    }
+    if(!is.null(txtFile)){
+        capture.output(me, file=txtFile, append=append, ...) 
+    }
 }
